21ebe95d22ffee0882abccd503d8ffbd0d3c2a64
Kaleb Jiménez <emilianokaleb@gmail.com>
2026-09-21 19:23:10 -0600

    feat(): limpieza de datos y analisis de reglas del dataset

--- config/rutas.py
+++ config/rutas.py
@@ -7,3 +7,4 @@ RUTA_PROYECTO = Path(__file__).resolve().parent.parent
 # Rutas de archivos de datos
 RUTA_DATA = RUTA_PROYECTO / "data"
 RUTA_DATA_RAW = RUTA_DATA / "data-raw"
+RUTA_DATA_PROCESSED = RUTA_DATA / "data-processed"
